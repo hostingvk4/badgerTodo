9b12f84edaa865d1f7ff468ae231586de713ac57
ivan <hostingvk4@mail.ru>
2021-08-25 15:50:30 +0400

    add server run functional

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"github.com/hostingvk4/badgerList/internal/handler"
 	"github.com/hostingvk4/badgerList/internal/repository"
 	"github.com/hostingvk4/badgerList/internal/repository/database"
 	"github.com/hostingvk4/badgerList/internal/server"
 	"github.com/hostingvk4/badgerList/internal/service"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -15,7 +20,18 @@ func main() {
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	srv := new(server.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occured while running http server: %s", err.Error())
+	go func() {
+		if err := srv.Run("8000", handlers.InitRoutes()); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
+	println("app start")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+	println("app shutting down")
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Fatalf("error occured on server shutting down: %s", err.Error())
 	}
 }
